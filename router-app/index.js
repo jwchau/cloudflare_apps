80ca84a165b0563f22c4e96dee1111a3bde76859
jwchau <john.win.chau@gmail.com>
2020-10-18 16:29:45 -0700

    clean up unused template routes

--- router-app/index.js
+++ router-app/index.js
@@ -27,15 +27,16 @@ function linkHandler(req) {
 async function handleRequest(request) {
     const r = new Router()
     // Replace with the appropriate paths and handlers
-    r.get('.*/bar', () => new Response('responding for /bar'))
-    r.get('.*/foo', request => handler(request))
-    r.post('.*/foo.*', request => handler(request))
-    r.get('/demos/router/foo', request => fetch(request)) // return the response from the origin
+    // r.get('.*/bar', () => new Response('responding for /bar'))
+    // r.get('.*/foo', request => handler(request))
+    // r.post('.*/foo.*', request => handler(request))
+    // r.get('/demos/router/foo', request => fetch(request)) // return the response from the origin
 
-    // custom route
-    r.get('/test', req => linkHandler(req))
-
+    // root route and links route
+    r.get('/links', req => linkHandler(req))
     r.get('/', () => new Response('Hello worker!')) // return a default message for the root route
+
+    // r.get('/', () => new Response('Hello worker!')) // return static html page on root
 
     const resp = await r.route(request)
     return resp
