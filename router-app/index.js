bef5dbb27922b1a6ed770d015e95132c7ca400c3
jwchau <john.win.chau@gmail.com>
2020-10-20 16:10:59 -0700

    move public out

--- router-app/index.js
+++ router-app/index.js
@@ -23,23 +23,23 @@ const urlRewriter = {
     },
 }
 
+const urlRewriter = {
+    element: (element) => {
+        element.setAttribute('href', 'https://workers.cloudflare.com')
+        element.setInnerContent('Return to Cloudflare Workers')
+    },
+}
+
 async function handleRequest(req) {
-    const url = req.url
-    console.log(url)
     const r = new Router()
 
     // root route and links route
     r.get('/links', req => handler(req))
+    r.get('/', () => fetch('https://static-links-page.signalnerve.workers.dev'))
 
-    // fetch static page
-    const html = fetch('https://static-links-page.signalnerve.workers.dev')
 
-    r.get('/', () => new Response(html, {
-        headers: {
-            "content-type": "text/html"
-        }
-    }))
 
+    console.log('request finish')
     const resp = await r.route(req)
     return resp
 }
