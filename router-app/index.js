a75ee2ddbcd4d0aace25bfa67a9c03c09a95f84e
jwchau <john.win.chau@gmail.com>
2020-10-20 15:10:14 -0700

    implement styles import

--- router-app/index.js
+++ router-app/index.js
@@ -16,7 +16,18 @@ function handler(req) {
     return new Response(body, init)
 }
 
-const html = require('./public/root')
+const {html, styles} = require('./public/root')
+
+class LinksTransformer {
+    constructor(links) {
+      this.links = links
+    }
+    
+    async element(element) {
+      // Your code
+    }
+  }
+
 async function handleRequest(req) {
     const url = req.url
     console.log(url)
@@ -25,15 +36,20 @@ async function handleRequest(req) {
     // root route and links route
     r.get('/links', req => handler(req))
 
-    // return a default message for the root route
-    r.get('/', () => new Response('Hello worker!')) 
+    // import stylesheet
+    r.get('/styles', () => new Response(styles, {
+        headers: {
+            "content-type": "text/css"
+        }
+    }))
 
     // return static html page on root
-    r.get('/html', () => new Response(html, {
+    r.get('/', () => new Response(html, {
         headers: {
             "content-type": "text/html;charset=UTF-8"
         }
     }))
+
 
     const resp = await r.route(req)
     return resp
